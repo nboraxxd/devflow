0ed05cbf7f161838d15db2fe8fcb589d5a963a54
bora <nhatbaoxxd@gmail.com>
2023-12-26 16:42:02 +0700

    Feat: Creating base routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <main className="flex-center min-h-screen w-full">{children}</main>
+}
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,5 @@
+export default function AskQuestion() {
+  return (
+    <div>AskQuestion</div>
+  )
+}
